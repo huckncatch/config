d03285e227000d84baf056b0e7a57836ce0372ba
Jim Mccarty <huckncatch@fastmail.com>
2024-01-26 22:43:10 -0800

    cleaned startup aliases

--- zsh/common-zshrc.zsh
+++ zsh/common-zshrc.zsh
@@ -1,5 +1,5 @@
 # Common user configuration
-echo "Common user configuration"
+# echo "Common user configuration"
 
 export LS_COLORS='bd=40;33;01:cd=40;33;01:di=01;34:ex=100;01;33:fi=00:ln=01;36:no=00:or=40;31;01:pi=40;33:so=01;35:*.aac=01;34:*.app=01;33:*.arj=01;31:*.asf=01;35:*.avi=01;35:*.bin=01;31:*.bmp=01;35:*.bz2=01;31:*.c=01;36:*.cgi=01;36:*.cpp=01;36:*.dmg=01;32:*.flac=01;34:*.gif=01;35:*.gz=01;31:*.h=01;37:*.hqx=01;31:*.jpg=01;35:*.JPG=01;35:*.js=01;36:*.lzh=01;31:*.m=01;36:*.m4a=01;34:*.m4v=01;35:*.md=01;37:*.mkv=01;35:*.mov=01;35:*.mp3=01;34:*.mp4=01;34:*.mpg=01;35:*.pdf=01;35:*.php=01;36:*.pl=01;36:*.png=01;35:*.sh=01;36:*.sit=01;31:*.sitx=01;31:*.smi=01;32:*.swift=01;36:*.tar=01;31:*.taz=01;31:*.tcl=01;36:*.tgz=01;31:*.tif=01;35:*.tiff=01;35:*.ts=01;36:*.webm=01;35:*.wmv=01;35:*.xml=01;37:*.yml=01;37:*.z=01;31:*.Z=01;31:*.zip=01;31:*.zsh=01;36:'
 export CLICOLOR=1
@@ -46,29 +46,6 @@ eval "$(rbenv init - zsh)"
 # Ruby 3.1 and above requires OpenSSL 3:
 # - brew install openssl@3 readline libyaml gmp
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@3)"
-
-## fix ls aliases
-# in common ??
-# alias ls='ls -FsC --color=auto --si'
-# alias lsd='ls -ld *(-/DN)'      # list directories only
-# alias lsda='ls -l *(-/DN)'      # list directories and their contents
-
-# alias grep=egrep
-# alias gfind='find . -type f -follow -print0 | xargs -0 grep -n'
-# alias so='source'
-# alias sorc='source ~/.zshrc'
-
-# alias eg='set | grep -i'
-# alias ag='alias | grep -i'
-# alias hg='history 1- | grep -i'
-
-# alias em='emacs'
-# alias yt='youtube-dl --cookies ./cookies.txt --recode-video mp4'
-# alias rmquarantine='xattr -d -r com.apple.quarantine'
-#
-# alias verifyCodeSign='codesign --verify --verbose'
-
-# export HOMEBREW_PREFIX="/opt/homebrew"
 
 export PATH="/usr/local/sbin:$PATH"
 
--- zsh/oh-my-zsh-custom/00_aliases.zsh
+++ zsh/oh-my-zsh-custom/00_aliases.zsh
@@ -1,4 +1,4 @@
-echo "oh-my-zsh/custom/00_aliases.zsh"
+# echo "oh-my-zsh/custom/00_aliases.zsh"
 
 if [[ -o interactive ]]; then
 
@@ -9,10 +9,12 @@ if [[ -o interactive ]]; then
   alias jobs='builtin jobs -l'
   alias mmv="noglob zmv -W"
 
+  # search aliases
   alias eg='set | grep -i'
   alias ag='alias | grep -i'
   alias cg='compctl | grep -i'
   alias hg='history 1- | grep -i'
+
   alias c=clear
 
 # doesn't work with sudo... do I really need nocorrect anyway?
@@ -24,16 +26,11 @@ if [[ -o interactive ]]; then
   alias em='emacs'
   alias so='source'
 
-  # deprecated, should use `exec zsh` instead
-  # alias sorc='so ~/.zshrc'
+  # restart shell
   alias ez='exec zsh'
 
   ## ls
-#   alias lg="grep 'alias l' $ZDOT/zshrc_local.d/aliases.mine.zsh"  # show all list aliases
-#   alias ll='ls -l'                    # long listing (no dot files)
-#   alias la='ls -lA'                   # long listing (full)
-#   alias lt='ls -lt'                   # long listing sorted by date
-  alias ltr='ls -ltr'                 # long listing sorted by date (reversed)
+  alias ltr='ls -ltrFH'                 # long listing sorted by date (reversed)
   alias l.='ls -ld .*'                # list dot files only
   # List only directories and symbolic links that point to directories
   alias lsd='ls -ld *(-/DN)'          # list directories only
@@ -42,7 +39,6 @@ if [[ -o interactive ]]; then
   alias grep='egrep'
 
   ## git
-  alias grhs='git reset --soft'
 #   alias gt='git tag'
   alias gpot='git push origin "$(git_current_branch)" && git push origin --tags'
   alias gfst='git fetch && git status'
@@ -66,4 +62,6 @@ if [[ -o interactive ]]; then
   ## homebrew
   alias bi='brew info'
   alias bs='brew search'
+  alias blc='brew list --cask'
+  alias bis='brew install'
 fi
