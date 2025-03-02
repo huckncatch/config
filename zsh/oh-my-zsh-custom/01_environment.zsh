1a7b777beb372afd775bc5a0ca009a8eb00e9b52
Jim Mccarty <huckncatch@fastmail.com>
2025-03-02 07:08:38 -0800

    updates
    unique arrays for path manpath fpath
    nvm updates

--- zsh/oh-my-zsh-custom/01_environment.zsh
+++ zsh/oh-my-zsh-custom/01_environment.zsh
@@ -47,6 +47,8 @@ eval "$(rbenv init - --no-rehash zsh)"
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@3)"
 
 ## nvm
-export NVM_DIR="$HOME/.nvm"
+# using the zsh-nvm plugin instead of the official nvm installation
+# NVM_DIR is still getting set somehow
+# export NVM_DIR="$HOME/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
